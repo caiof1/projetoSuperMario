7072a30111f542c0f0481cb5520c356dd77424ba
Caio Fernandes <caionl2071@gmail.com>
2023-02-15 12:15:16 -0300

    second commit

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,23 @@
+// Função para iniciar o jogo
 function startGame() {
     location.href = 'game.html'
 }
+// Funçao para abrir o como jogar
 function howPlay() {
     document.getElementById('menu').style.display = 'none'
     document.getElementById('how').style.display = 'block'
 }
+// Função para voltar ao menu
 function voltar() {
     document.getElementById('menu').style.display = 'block'
     document.getElementById('how').style.display = 'none'
 }
+// Função para entrar na aba help
 function help() {
     document.getElementById('menu').style.display = 'none'
     document.getElementById('help').style.display = 'block'
 }
+// Função para sair da aba help
 function voltarHelp() {
     document.getElementById('menu').style.display = 'block'
     document.getElementById('help').style.display = 'none'
