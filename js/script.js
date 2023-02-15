f4a931c0340a3d4bf73918e49138c6c7b3497513
Caio Fernandes <caionl2071@gmail.com>
2023-02-15 12:11:13 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,38 +1,52 @@
+// Iniciando variaveis de pulo, do cano, do mario e pontos
 let pulou = false
 const pipe = document.getElementById('pipe')
 let mario = document.getElementById('mario')
 let pontos = 0
 
+// uma function para pegar a letra, verificar se a tecla apertada é a 32, no caso, o space, e efetuar o pulo
 let pegarLetra = () => {
 
     function teclando() {
+        // Pegando o evento keycode
         let teclaPega = event.keyCode
+        // Verificando se a tecla pega é a 32 e se o usuario já pulou
         if(teclaPega == 32 && pulou == false) {
             mario.className = 'mario pulo'
             pulou = true
+            // Caso entre nessa condição, depois de 1 segundo, o mario cai e pode pular novamente
             setTimeout(() => {
                 mario.className = 'mario'
                 pulou = false
             }, 1000);
         }
     }
+    // atribuindo ao body, o evento onkeypress, recebendo a function teclado
 
     document.body.onkeypress = teclando;
 
 }
+// Criando variaveis da nuvem e da posição do mario
 let printNuvem = true
 let positionMario
+
+// Criando um loop para fazer a verificação a cada 10 milisegundos
 let loop = setInterval(() => {
+    // Pegando a posição left do cano
     const pipePosition = pipe.offsetLeft
     
+    // usando o try catch, para tentar pegar a posição top do mario
     try {
         positionMario = document.getElementById('mario').offsetTop
     } catch (e) {
         
     }
-    let adicionarAnimacao = true 
 
+    // Variavel responsavel por adicionar animação no mario
+    let adicionarAnimacao = true 
+    // Uma condição para verificar se o mario encostou no cano
     if((pipePosition <= 85 && pipePosition >= -25) && positionMario > 305 && adicionarAnimacao == true ) {
+            // Parando tudo na tela para que nada mais se mexa enquanto o game está na tela de defeat
             pipe.style.animation = 'nome'
             pipe.style.left = `${pipePosition}px`
             mario.style.animation = 'none'
@@ -40,16 +54,18 @@ let loop = setInterval(() => {
             mario.src = 'img/game-over.png'
             mario.width = '50'
             mario.style.left = '30px'
-            
+
+            // Parando a contagem de pontos
             clearInterval(contarPonto)
 
 
             mostrarPontos()
 
-            
+            // Pegando as duas nuvens
             let nuvem1 = document.getElementById('nuvem1')
             let nuvem2 = document.getElementById('nuvem2')
 
+            // Condição que verifica se a variavel printNuvem é true, e trava as nuvens na posição atual
             if(printNuvem) { 
                 let positionNuvem1 = nuvem1.offsetLeft
                 let positionNuvem2 = nuvem2.offsetLeft
@@ -58,12 +74,13 @@ let loop = setInterval(() => {
                 nuvem2.style.left = `${positionNuvem2}px`
                 printNuvem = false
             }
+            // Retira as animation da nuvem
             nuvem1.style.animation = 'none'
             nuvem2.style.animation = 'none'
             
-
+            // Fazendo conexão com o css
             styleSheet = document.getElementById('style')
-
+            // Criando uma animação utilizando o insertRule
             styleSheet.sheet.insertRule(`@keyframes morte {
                 0% {
                     top: ${positionMario + 15}px;
@@ -82,8 +99,10 @@ let loop = setInterval(() => {
                 }
             }`)
 
+            // Adicionado a animação de morte
             mario.style.animation = 'morte infinite linear 2s'
 
+            // Remover o mario e parar o loop depois de 2 segundos
             setTimeout(() => {
                 mario.remove()
                 clearInterval(loop)
@@ -98,10 +117,12 @@ let loop = setInterval(() => {
 }, 10);
 
 pegarLetra()
+
+// função para reiniciar o game
 function reiniciarGame() {
     location.reload()
 }
-
+// função para fazer a contagem e mostrar na tela os pontos 
 let contarPonto = setInterval(() => {
     pontos++
     document.getElementById('pontos').innerHTML = pontos
